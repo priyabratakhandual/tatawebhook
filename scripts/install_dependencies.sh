f0f33d330baf31fbc9a51ff3a2d67f188228e229
priyabratakhandual <priyabratakhandual7@gmail.com>
2026-03-13 16:20:34 +0530

    fix virtualenv install for codedeploy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+set -e
+
 cd /home/ubuntu/tata-webhook
 
-python3 -m venv venv
+# create virtual environment if it does not exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
 
+# activate virtual environment
 source venv/bin/activate
 
+# upgrade pip
 pip install --upgrade pip
 
+# install project dependencies
 pip install -r requirements.txt
